af46e263414294e44c0587dd6bfc289a8122390a
MaxKing89 <robert.king@chicos.com>
2016-05-12 09:40:54 -0400

    pull data into its own file

--- platforms/browser/www/js/video.js
+++ platforms/browser/www/js/video.js
@@ -1,62 +1,6 @@
-//src: code comes after v=
 
-var allVideoArr = [
-     {
-        name: 'Pizza Bread Bowl',
-        src: 'Eh5_ssE_BTY',
-        ing:['Cheese','1 bread bowl','1 cup marinara sauce','8 ounces fresh mozzarella','6 ounces pepperoni','½ sliced onion','½ cup basil','1 cup cooked sausage','1 sliced green bell pepper','1 cup shredded white cheddar'],
-        cuisineType: 'Italian'
-     },
-     {
-        name: 'Slow Cooker Ribs',
-        src: '1t60widjlhY',
-        ing:['2 cups of bbq sauce','1/4 cup of brown sugar','4 Tbsp. of cider vinegar','3 tsp of oregano','1 tsp of Worcestershire sauce','1 Tbsp. of cayenne pepper','1 Tbsp. of chili powder','3 lbs of baby back ribs','salt and pepper'],
-        cuisineType: 'American'
-    },
-     {
-        name: 'Chili Cheese Dog Bake',
-        src: 'zD2KF7uHY4E',
-        ing:['1 yellow onion, diced', '1 pound ground beef', '½ teaspoon salt', '½ teaspoon pepper', '½ cup tomato sauce', '½ cup ketchup', '2 ½ teaspoons chili powder', '½ teaspoon sugar', '10 hot dogs', '1 can pre-made biscuits', '4 tablespoons butter, melted', '½ teaspoon garlic powder', '2 tablespoons parsley', '1 cup shredded cheddar cheese'],
-        cuisineType: 'American'
-    },
-     {
-        name: 'Mini S’mores Pies',
-        src: '0HXaRaxNHDg',
-        ing: ['<b>Graham Cracker Crust:</b>','2 packets of graham crackers, finely crushed','1 stick unsalted butter, melted','⅓ cup granulated sugar','<br><b>Chocolate Filling:</b>','1 ¼ cup of heavy cream','4 tablespoons unsalted butter, cubed','⅓ cup granulated sugar','10.5oz / 300g dark chocolate (60% cocoa minimum) chopped','3 eggs, plus 2 yolks','20 large marshmallows ','Foil cupcake liners','Muffin or cupcake pan'],
-        cuisineType: 'Dessert'
-    },
-     {
-        name: 'No-Bake Peanut Butter Pie Jars',
-        src: '1r9A9u1V6SQ',
-        ing: ['1 cup powdered sugar', '8 ounces cream cheese, softened', '1 cup creamy peanut butter', '16 ounces whipped topping, divided', 'Graham crackers', 'Hard shell chocolate sauce', '<br>', '<b>optional toppings:</b>', 'crushed peanuts', 'rainbow sprinkles', 'mini chocolate chips', '<br>', '12 jam jars '],
-        cuisineType: 'Dessert'
-    },
-    {
-        name: 'Peanut Butter Lava Cake',
-        src: '1SnCh32hEDI',
-        ing: ['200g dark chocolate, chopped', '100g unsalted butter, chopped', '2 eggs', '2 egg yolks', '1/2 cup (110 gr ) caster (superfine) sugar', '1/4 cup (35 gr) plain (all- purpose) flour, sifted', 'peanut butter', 'Cocoa + Butter or cooking spray, for coating', 'Berries ( for decoration) '],
-        cuisineType: 'Dessert'
-    },
-     {
-        name: 'Asian Chicken Lettuce Wraps',
-        src: 'gLib-c3_rUw',
-        ing: ['None Listed :('],
-        cuisineType: 'Chinese'
-    },
-     {
-        name: 'Cucumber, Tomato, and Avocado Salad',
-        src: 'vUBpiP9aPtg',
-        ing: ['- 1 English cucumber', '- 4 Roma tomatoes', '- 3 ripe avocados', '- 1/2 red onion', '- 1/4 cup cilantro', '- Juice of 1 lemon', '- salt and black pepper to taste', '- 2 Tbsp. extra virgin olive oil'],
-        cuisineType: 'Other'
-    },
-    {
-        name: 'Southwestern Taco Salad',
-        src: 'cyeoZwMLPSw',
-        ing: ['<b>Dressing:</b>', '1/2 an avocado', '1/2 cup chopped cilantro', '1 Tablespoon lime juice', '1/2 cup plain low fat greek yogurt', '1/3 cup water', '1/2 tsp salt', '1/2 Tablespoon garlic', '1/2 Tablespoon cumin', '<b>Salad:</b>', '1 tbsp olive oil', '1 chicken breast, diced', '1/2 packet of taco seasoning', '1 head Romaine Lettuce, chopped', '1 cup shredded cheddar jack cheese', '1 cup black beans', '1 avocado, diced', '1 cup corn', '1 cup cherry tomatoes, halved', '1 cup tortilla strips'],
-        cuisineType: 'Mexican'
-    }
-];
 
+//if the parameter after ? in the url is equal to cuisine type put in separate array and rename allVideoArr
 if (location.search){
     allVideoArr = allVideoArr.filter(function(obj) {
     if(location.search.indexOf(obj.cuisineType) > -1 ){
@@ -66,7 +10,7 @@ if (location.search){
 });
 }
 
-
+//grab random video from results set
 var randomResultSet = allVideoArr[Math.floor(Math.random()*allVideoArr.length)];
 
 var getName = randomResultSet.name;
@@ -77,28 +21,38 @@ $('.cuisine-type').html("<h2>Cuisine Type - " + getCuisineType + "</h2>")
 
 var getSRC = "https://www.youtube.com/embed/" + randomResultSet.src + "?autoplay=1";
 
+//set src
 $('#video-player').attr('src', getSRC).animate({top:'200px', opacity:1}, function(){ $(this).attr('autoplay', 'true') });
 
 var getIngrediants = randomResultSet.ing.join('<br>');
 $('.ingredients').html("<h3>Ingredients - </h3><p>" + getIngrediants + "</p>")
 
+
+//create variable for the current sort param
 var currentFilter = location.search;
 
-
-//if $(.again).(function(){
-    //$(this).attr('href', function() {
-    //return this.href + currentFilter;
-    //});
-//});
+//if user hits the go again button keep the current parameters
+if ($('.again a.button')[0]){
+    $('.again a.button').attr('href', function() {
+    return this.href + currentFilter;   
+    });
+};
 
 
 ////////////////////////////////////////////////FUNCTIONS/////////////////////////////////////////
 
+//append the id of the selected button to the url after ? so we can sort the result set
 $(function(){
    $('#types a.button').each(function(){
        $(this).attr('href', function(){
            return this.href + '?' + $(this).attr('id') + '=true';
        });
    });
+    //if user hits the go again button keep the current parameters
+if ($('.again a.button')[0]){
+    $('.again a.button').attr('href', function() {
+    return this.href + currentFilter;   
+    });
+};
 });
 
